9c2ea924cf6e2fc21e19a2ac460d48f561bcf8c8
EdMT220 <97614174+AndreBoi02@users.noreply.github.com>
2024-05-03 13:32:10 -0600

    Unity Files

---
+++ Assets/Scripts_A/SteeringBehaviours.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteeringBehaviours {
+    void seek() {
+
+    }
+
+    void flee() {
+
+    }
+
+    void arrive() {
+
+    }
+
+    void pursuit() {
+
+    }
+
+    void evade() {
+
+    }
+
+    void wander() {
+
+    }
+}
