adc1e7242f9fe292a4331b79d3acd6da298df963
DatBoi02 <97614174+AndreBoi02@users.noreply.github.com>
2024-05-06 05:26:54 -0600

    HomeWork Commit
    Wander needs to be fixed

--- Assets/Scripts_A/BasicAgent.cs
+++ Assets/Scripts_A/BasicAgent.cs
@@ -4,19 +4,71 @@ using UnityEngine;
 
 public class BasicAgent : MonoBehaviour {
 
+    #region Vars
+    public Vector3 m_pos;
+    public Vector3 m_currentVel;
+    public float m_maxVel;
+    public float m_maxForce;
+    public float m_mass;
+    public float m_maxSpeed;
+    public float m_slowingFactor;
+    public float m_proximity;
+    public BasicAgent aTarget;
+    public Vector3 m_targetPos;
+    #endregion
+
     SteeringBehaviours behaviours;
 
     [SerializeField] Vector3 eyesPos;
+    [SerializeField] List<Transform> points2Follow = new List<Transform>();
     [SerializeField] float eyesRad;
+    [SerializeField] string targetNameA;
+    [SerializeField] string targetNameV;
 
-    // Start is called before the first frame update
     void Start() {
         behaviours = new SteeringBehaviours();
+        m_pos = transform.position;
+        if (targetNameA != "") {
+            aTarget = GameObject.Find(targetNameA).GetComponent<BasicAgent>();
+        }
+        if (targetNameV != "") {
+            m_targetPos = GameObject.Find(targetNameV).transform.position;
+        }
     }
 
-    // Update is called once per frame
     void FixedUpdate() {
-        perceptionManager();
+        Move();
+        if (targetNameV == "") {
+            return;
+        }
+        m_targetPos = GameObject.Find(targetNameV).transform.position;
+        //perceptionManager();
+    }
+
+    void Move() {
+        switch (type) {
+            case typeOfBehaviours.Seek:
+                behaviours.seek(this);
+                break;
+
+            case typeOfBehaviours.Flee:
+                behaviours.flee(this);
+                break;
+            case typeOfBehaviours.Pursuit:
+                behaviours.pursuit(this);
+                break;
+            case typeOfBehaviours.Evade:
+                behaviours.evade(this);
+                break;
+            case typeOfBehaviours.Wander:
+                //Needs to be fixed
+                behaviours.wander(this, 100, 500);
+                break;
+            case typeOfBehaviours.FollowPath:
+                behaviours.followingPath(this, points2Follow, m_proximity);
+                break;
+        }
+        transform.position = m_pos;
     }
 
     void perceptionManager() {
@@ -36,4 +88,15 @@ public class BasicAgent : MonoBehaviour {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(eyesPos, eyesRad);
     }
+
+    public enum typeOfBehaviours {
+        Seek,
+        Flee,
+        Pursuit,
+        Evade,
+        Wander,
+        FollowPath
+    }
+
+    public typeOfBehaviours type = typeOfBehaviours.Seek;
 }
--- Assets/Scripts_A/SteeringBehaviours.cs
+++ Assets/Scripts_A/SteeringBehaviours.cs
@@ -3,27 +3,111 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SteeringBehaviours {
-    void seek() {
-
+    public void seek(BasicAgent t_agent) {
+        Vector3 desiredVel = t_agent.aTarget.m_pos - t_agent.m_pos;
+        float distance = distanceV(t_agent.m_pos, t_agent.aTarget.m_pos);
+        baseBehaviour(desiredVel, t_agent);
+        arrive(t_agent, distance);
+        t_agent.m_pos += t_agent.m_currentVel;
     }
 
-    void flee() {
-
+    public void seek(BasicAgent t_agent, Vector3 t_targetV)
+    {
+        Vector3 desiredVel = t_targetV - t_agent.m_pos;
+        float distance = distanceV(t_agent.m_pos, t_targetV);
+        baseBehaviour(desiredVel, t_agent);
+        arrive(t_agent, distance);
+        t_agent.m_pos += t_agent.m_currentVel;
     }
 
-    void arrive() {
-
+    public void flee(BasicAgent t_agent) {
+        Vector3 desiredVel = t_agent.m_pos - t_agent.aTarget.m_pos;
+        baseBehaviour(desiredVel, t_agent);
+        t_agent.m_pos += t_agent.m_currentVel;
     }
 
-    void pursuit() {
-
+    void arrive(BasicAgent t_agent, float t_distance) {
+        if (t_distance <= t_agent.m_slowingFactor)
+        {
+            float slowing = t_distance / t_agent.m_slowingFactor;
+            t_agent.m_currentVel *= slowing;
+        }
     }
 
-    void evade() {
-
+    public void pursuit(BasicAgent t_agent) {
+        float T = 3;
+        Vector3 futurePos = t_agent.aTarget.m_pos + (t_agent.aTarget.m_currentVel * T);
+        t_agent.aTarget.m_pos = futurePos;
+        seek(t_agent);
     }
 
-    void wander() {
-
+    public void evade(BasicAgent t_agent) {
+        float T = 3;
+        Vector3 futurePos = t_agent.aTarget.m_pos + (t_agent.aTarget.m_currentVel * T);
+        t_agent.aTarget.m_pos = futurePos;
+        flee(t_agent);
     }
+
+    public void wander(BasicAgent t_agent, float t_disp, float t_radius) {
+        Vector3 newWanderPos = t_agent.m_currentVel;
+        newWanderPos = newWanderPos.normalized;
+        newWanderPos *= t_disp;
+        Vector3 vRandom = randomVector();
+        vRandom *= t_radius;
+        newWanderPos += vRandom;
+        newWanderPos += t_agent.m_pos;
+        float distance = distanceV(t_agent.m_pos, newWanderPos);
+        seek(t_agent, newWanderPos);
+        if (distance <= t_agent.m_proximity) {
+            
+        }
+        //My new implementation to make it work
+       
+    }
+
+    int currentNode = 0;
+    public void followingPath(BasicAgent t_agent, List<Transform> t_list, float t_proximity) {
+        float distance = distanceV(t_agent.m_pos, t_list[currentNode].position);
+        if (distance <= t_proximity) {
+            currentNode++;
+            if (currentNode >= t_list.Count) {
+                currentNode = t_list.Count - 1;
+                Debug.Log("Llegamos al último");
+            }
+        }
+        seek(t_agent, t_list[currentNode].position);
+    }
+
+    void baseBehaviour(Vector3 t_desiredVel, BasicAgent t_agent) {
+        t_desiredVel = t_desiredVel.normalized;
+        t_desiredVel *= t_agent.m_maxVel;
+        Vector3 steering = t_desiredVel - t_agent.m_currentVel;
+        steering = truncateVec(steering, t_agent.m_maxForce);
+        steering /= t_agent.m_mass;
+        t_agent.m_currentVel = truncateVec(t_agent.m_currentVel + steering, t_agent.m_maxSpeed);
+    }
+
+    #region Necesary Functions
+    Vector3 truncateVec(Vector3 t_v, float t_limit) {
+        //Start of truncate function between a Vector and a limit
+        Vector3 res = t_v;
+        if (res.magnitude <= t_limit)
+        {
+            return res;
+        }
+        res = res.normalized;
+        res *= t_limit;
+        return res;
+        //End of truncate function
+    }
+
+    float distanceV(Vector3 t_this, Vector3 t_other) {
+        return Mathf.Sqrt(Mathf.Pow((t_this.x - t_other.x), 2) + Mathf.Pow((t_this.y - t_other.y), 2) + Mathf.Pow((t_this.z - t_other.z), 2));
+    }
+
+    Vector3 randomVector() {
+        Vector3 ret = new Vector3(Random.Range(-1, 1.1f), Random.Range(-1, 1.1f), Random.Range(-1, 1.1f));
+        return ret;
+    }
+    #endregion
 }
