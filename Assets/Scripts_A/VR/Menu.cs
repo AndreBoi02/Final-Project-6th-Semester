d3813840ef764924ccc82b9a0936cb574a8ed069
AndreBoi <a.puchie2@gmail.com>
2024-05-08 08:50:55 -0600

    Almost

--- Assets/Scripts_A/VR/Menu.cs
+++ Assets/Scripts_A/VR/Menu.cs
@@ -3,24 +3,30 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour {
     [SerializeField] GameObject menu;
     [SerializeField] InputActionProperty inputAction;
 
+    [SerializeField] Scrollbar scrollbar;
+    
+    [SerializeField] GameObject xr;
+
     private void Start() {
         inputAction.action.performed += turnON;
     }
-
-    private void Update() {
-        
-    }
-
+    
     void turnON(InputAction.CallbackContext context) {
         menu.SetActive(!menu.activeSelf);
     }
 
-    void swtichC() {
-        
+    public void swtichC() {
+        if (scrollbar.value == 1) {
+            
+        }
+        if (scrollbar.value == 0) {
+            
+        }
     }
 }
