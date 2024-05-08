b797d5f763d46f41f7cfb4bab1dc853c62df5972
BOI <97614174+AndreBoi02@users.noreply.github.com>
2024-05-08 10:44:00 -0600

    Finished

--- Assets/Scripts_A/VR/Menu.cs
+++ Assets/Scripts_A/VR/Menu.cs
@@ -4,6 +4,7 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit.Locomotion.Turning;
 
 public class Menu : MonoBehaviour {
     [SerializeField] GameObject menu;
@@ -11,10 +12,13 @@ public class Menu : MonoBehaviour {
 
     [SerializeField] Scrollbar scrollbar;
     
-    [SerializeField] GameObject xr;
+    [SerializeField] SnapTurnProvider snap;
+    [SerializeField] ContinuousTurnProvider continuous;
 
     private void Start() {
         inputAction.action.performed += turnON;
+        snap = gameObject.GetComponent<SnapTurnProvider>();
+        continuous = gameObject.GetComponent<ContinuousTurnProvider>();
     }
     
     void turnON(InputAction.CallbackContext context) {
@@ -23,10 +27,12 @@ public class Menu : MonoBehaviour {
 
     public void swtichC() {
         if (scrollbar.value == 1) {
-            
+            continuous.enabled = false;
+            snap.enabled = true;
         }
         if (scrollbar.value == 0) {
-            
+            continuous.enabled = true;
+            snap.enabled = false;
         }
     }
 }
